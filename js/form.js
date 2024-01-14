ea889634b355bc4394a45deb1d744ce26013f2da
imqngali <55797666+imqngali@users.noreply.github.com>
2024-01-15 05:12:29 +0600

    150124

---
+++ js/form.js
@@ -0,0 +1,20 @@
+const form = document.querySelector('form');
+
+form.addEventListener('submit', (e) =>{
+    e.preventDefault();
+    const grecres = grecaptcha.getResponse();
+
+    if (!grecres.length > 0) {
+        let ele = document.getElementById('recaptcha_error');
+        ele.innerHTML += 'Пройдите проверку';
+        throw new Error("captcha")
+        
+    }
+});
+
+IMask(
+    document.getElementById('phone-mask'),
+    {
+      mask: '+{7}(000)000-00-00'
+    }
+  )
