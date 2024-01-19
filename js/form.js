a1cd067d9d4f91cd4595efb4b9c35b97f702df2a
imqngali <55797666+imqngali@users.noreply.github.com>
2024-01-19 21:11:23 +0600

    formfixga190124

--- js/form.js
+++ js/form.js
@@ -1,28 +1,35 @@
-const form = document.querySelector('form');
+const reload = document.getElementById('close-form');
 
-form.addEventListener('submit', (e) =>{
+reload.addEventListener('click', (e) => {
     e.preventDefault();
-    const grecres = grecaptcha.getResponse();
+    location.reload();
+})
 
-    if (!grecres.length < 0) {
-        const fb = new FormData(e.target);
-        const params = new URLSearchParams(fb);
 
-        fetch('telegram.php/post', {
-            method: "POST",
-            body: params
-        }) 
+const form_btn = document.getElementById('form-btn');
 
-        document.location.href = 'https://webmonsters.kz/thank-you.html'
-    }
+form_btn.addEventListener('click', (e) => {
+    e.preventDefault();
 
-    if (!grecres.length > 0) {
-        let ele = document.getElementById('recaptcha_error');
-        ele.innerHTML += 'Пройдите проверку';
-        throw new Error("captcha")
-        
-    }
-});
+    const form = document.getElementById('sait_form');
+    form.classList.add('disabled');
+
+    const captcha_wrap = document.getElementById('captcha');
+    const captcha = document.getElementById('captcha_btn');
+
+    captcha.addEventListener('click', (e) => {
+        e.preventDefault();
+        const grecres = grecaptcha.getResponse();
+
+        if(!grecres.length > 0) {
+            let ele = document.getElementById('recaptcha_error');
+            ele.innerHTML += 'Пройдите проверку';
+        } else {
+            captcha_wrap.classList.add('disabled');
+            form.classList.remove('disabled');
+        }
+    })
+})
 
 IMask(
     document.getElementById('phone-mask'),
