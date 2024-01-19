61bcc32e8aa91d26def1ddbbfdff524dbbdb7b65
imqngali <55797666+imqngali@users.noreply.github.com>
2024-01-19 22:57:46 +0600

    anotherformfix

--- js/form.js
+++ js/form.js
@@ -6,28 +6,30 @@ reload.addEventListener('click', (e) => {
 })
 
 
-const form_btn = document.getElementById('form-btn');
+const form_btn = document.querySelectorAll('#form-btn');
 
-form_btn.addEventListener('click', (e) => {
-    e.preventDefault();
-
-    const form = document.getElementById('sait_form');
-    form.classList.add('disabled');
-
-    const captcha_wrap = document.getElementById('captcha');
-    const captcha = document.getElementById('captcha_btn');
-
-    captcha.addEventListener('click', (e) => {
+form_btn.forEach(f => {
+    f.addEventListener('click', (e) => {
         e.preventDefault();
-        const grecres = grecaptcha.getResponse();
-
-        if(!grecres.length > 0) {
-            let ele = document.getElementById('recaptcha_error');
-            ele.innerHTML += 'Пройдите проверку';
-        } else {
-            captcha_wrap.classList.add('disabled');
-            form.classList.remove('disabled');
-        }
+    
+        const form = document.getElementById('sait_form');
+        form.classList.add('disabled');
+    
+        const captcha_wrap = document.getElementById('captcha');
+        const captcha = document.getElementById('captcha_btn');
+    
+        captcha.addEventListener('click', (e) => {
+            e.preventDefault();
+            const grecres = grecaptcha.getResponse();
+    
+            if(!grecres.length > 0) {
+                let ele = document.getElementById('recaptcha_error');
+                ele.innerHTML += 'Пройдите проверку';
+            } else {
+                captcha_wrap.classList.add('disabled');
+                form.classList.remove('disabled');
+            }
+        })
     })
 })
 
